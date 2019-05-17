017164a8877f7dd0e9459dc1df5868ea162e6a8f
ggranddev <blue.webking960112@gmail.com>
2019-05-16 23:18:45 -1000

    Integrate message service to item  service

--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { ITEMS } from "./mockup";
 import { Item } from './item';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getItems(): Observable<Item[]> {
+    this.messageService.add(('Item Service is fetching Items'));
   	return of(ITEMS);
   }
 }
