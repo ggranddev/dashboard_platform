5e4b47703fa0a246535d30528b5aeaa8247b0374
ggranddev <blue.webking960112@gmail.com>
2019-05-16 22:36:40 -1000

    Add service for getting item list

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -10,15 +10,19 @@ import { ItemService } from '../item.service';
   styleUrls: ['./items.component.scss']
 })
 export class ItemsComponent implements OnInit {
-	items: Item[] = ITEMS;
+	items: Item[];
 	selectedItem: Item;
-  	constructor() { }
+  	constructor(private itemService: ItemService) { }
 
   	ngOnInit() {
-
+      this.getItems();
   	}
 
   	onSelect(item: Item) {
   		this.selectedItem = item;
   	}
+
+  	getItems() {
+      this.items = this.itemService.getItems();
+    }
 }
